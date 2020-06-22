3c9887613881fbe427ef2926f9640cb279d6a496
Guo Y.K <hi@guoyk.net>
2020-06-22 16:00:16 +0800

    complete

---
+++ main.go
@@ -0,0 +1,115 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"github.com/aliyun/aliyun-oss-go-sdk/oss"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+)
+
+func exit(err *error) {
+	if *err != nil {
+		log.Println("exited with error:", (*err).Error())
+		os.Exit(1)
+	} else {
+		os.Exit(0)
+	}
+}
+
+type Conf struct {
+	Workspace          string `json:"workspace"`
+	OSSPublicURL       string `json:"oss_public_url"`
+	OSSEndpoint        string `json:"oss_endpoint"`
+	OSSAccessKeyID     string `json:"oss_access_key_id"`
+	OSSAccessKeySecret string `json:"oss_access_key_secret"`
+	OSSBucket          string `json:"oss_bucket"`
+}
+
+var (
+	optConf string
+	optURL  string
+)
+
+func main() {
+	var err error
+	defer exit(&err)
+
+	flag.StringVar(&optConf, "c", "/etc/ossmirror/config.json", "configuration file")
+	flag.StringVar(&optURL, "l", "", "url to download")
+	flag.Parse()
+
+	optURL = strings.TrimSpace(optURL)
+
+	if optURL == "" {
+		err = errors.New("invalid url")
+		return
+	}
+
+	var buf []byte
+	if buf, err = ioutil.ReadFile(optConf); err != nil {
+		return
+	}
+
+	var conf Conf
+	if err = json.Unmarshal(buf, &conf); err != nil {
+		return
+	}
+
+	log.Println("oss go sdk version: ", oss.Version)
+
+	var client *oss.Client
+	if client, err = oss.New(conf.OSSEndpoint, conf.OSSAccessKeyID, conf.OSSAccessKeySecret); err != nil {
+		return
+	}
+
+	log.Println("bucket:", conf.OSSBucket)
+
+	var bucket *oss.Bucket
+	if bucket, err = client.Bucket(conf.OSSBucket); err != nil {
+		return
+	}
+
+	log.Println("url:", optURL)
+
+	var uri *url.URL
+	if uri, err = url.Parse(optURL); err != nil {
+		return
+	}
+
+	filename := filepath.Join(conf.Workspace, path.Base(uri.Path))
+	log.Println("local file:", filename)
+	defer os.Remove(filename)
+
+	var f *os.File
+	if f, err = os.OpenFile(filename, os.O_CREATE|os.O_RDWR, 0640); err != nil {
+		return
+	}
+	defer f.Close()
+
+	var res *http.Response
+	if res, err = http.Get(optURL); err != nil {
+		return
+	}
+	defer res.Body.Close()
+
+	if _, err = io.Copy(f, res.Body); err != nil {
+		return
+	}
+
+	key := path.Join(uri.Host, uri.Path)
+	log.Println("remote file:", path.Join(conf.OSSPublicURL, key))
+	if err = bucket.PutObjectFromFile(key, filename); err != nil {
+		return
+	}
+
+	log.Println("done")
+}
